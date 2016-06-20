44ff8c44e053c50e1380455af7999d85b3e11a20
Torbjörn Rathsman <milasudril@gmail.com>
2016-06-20 13:59:47 +0200

    Added dependency class

---
+++ dependency.cpp
@@ -0,0 +1,11 @@
+//@ {
+//@ "targets":[{"name":"dependency.o","type":"object"}]
+//@ }
+
+#include "dependency.hpp"
+
+using namespace Maike;
+
+Dependency::Dependency(const char* name,Relation relation):
+	m_name(name),r_target(nullptr),m_rel(relation)
+	{}
---
+++ dependency.hpp
@@ -0,0 +1,52 @@
+//@ {
+//@ "targets":[ {"name":"dependency.hpp","type":"include"} ]
+//@ "dependencies_extra":[{"ref":"dependency.o","rel":"implementation"}]
+//@ }
+
+#ifndef MAIKE_DEPENDENCY_HPP
+#define MAIKE_DEPENDENCY_HPP
+
+#include "target.hpp"
+#include <string>
+
+namespace Maike
+	{
+	class Dependency
+		{
+		public:
+			enum class Relation:unsigned int
+				{
+				 INTERNAL
+				,IMPLEMENTATION
+				,EXTERNAL
+				};
+
+			explicit Dependency(const char* name,Relation relation);
+			Dependency(const Dependency&)=default;
+            Dependency& operator=(const Dependency&)=default;
+
+			const char* nameGet() const noexcept
+				{
+				return r_target==nullptr?
+					m_name.c_str() : r_target->nameGet();
+				}
+
+			Target* target() noexcept
+				{return r_target;}
+
+			const Target* target() const noexcept
+				{return r_target;}
+
+			Relation relationGet() const noexcept
+				{return m_rel;}
+
+
+
+		private:
+			std::string m_name;
+			Target* r_target;
+			Relation m_rel;
+		};
+	}
+
+#endif
