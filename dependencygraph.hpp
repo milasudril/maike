cd0347a831e1a3461972459d7502c21862bb354e
Torbjörn Rathsman <milasudril@gmail.com>
2016-06-20 18:55:31 +0200

    Added DependencyGraph interface

---
+++ dependencygraph.hpp
@@ -0,0 +1,31 @@
+//@ {
+//@ "targets":[{"name":"dependencygraph.hpp","type":"include"}
+//@ }
+
+#ifndef MAIKE_DEPENDENCYGRAPH_H
+#define MAIKE_DEPENDENCYGRAPH_H
+
+#include <memory>
+
+namespace Maike
+	{
+	class Target;
+
+	class DependencyGraph
+		{
+		public:
+			virtual DependencyGraph& targetRegister(std::unique_ptr<Target>&& target)=0;
+			virtual DependencyGraph& targetsPatch()=0;
+
+			class TargetVisitor
+				{
+				public:
+					virtual void visit(DependencyGraph& graph,Target& target)=0;
+				};
+
+			virtual void targetsVisit(TargetVisitor&& visitor)=0;
+			virtual Target* targetFind(const char* name)=0;
+		};
+	}
+
+#endif
