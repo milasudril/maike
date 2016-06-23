64dc5585fbb201c3ac44fb0a3b72d96f06af78d8
Torbjörn Rathsman <milasudril@gmail.com>
2016-06-23 15:15:36 +0200

    DirectoryLister

---
+++ directorylister.hpp
@@ -0,0 +1,24 @@
+//@{
+//@"targets":[{"name":"directorylister.hpp","type":"include"}]
+//@"dependencies_extra":[{"ref":"directorylister.o",rel:"implementation"}]
+//@}
+
+#ifndef MAIKE_DIRECTORYLISTER_H
+#define MAIKE_DIRECTORYLISTER_H
+
+namespace Maike
+    {
+    class DirectoryLister
+        {
+        public:
+            DirectoryLister(const char* dirname);
+            ~DirectoryLister();
+            const char* read();
+
+        private:
+            struct Impl;
+            Impl* m_impl;
+        };
+    }
+
+#endif
