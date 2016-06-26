bab627848c410962e192d03956c0223f95796e64
Torbjörn Rathsman <milasudril@gmail.com>
2016-06-26 11:43:49 +0200

    Initial work on TargetCxx

--- targetcxxoptions.hpp
+++ targetcxxoptions.hpp
@@ -12,6 +12,11 @@ namespace Maike
 		private:
 			std::vector< std::string > m_include_dir;
 			std::vector< std::string > m_lib_dir;
+			std::string m_platform_prefix;
+			Command m_app_cmd;
+			Command m_lib_cmd;
+			Command m_dll_cmd;
+			Command m_object_cmd;
 		};
 	}
 
