f793d297c6acf239c51234a6c1db1f5e595a60b7
Torbjörn Rathsman <milasudril@gmail.com>
2016-07-08 20:56:34 +0200

    Parse /proc/cpuinfo

--- sysvars-linux.cpp
+++ sysvars-linux.cpp
@@ -10,6 +10,8 @@
 #include "stringkey.hpp"
 #include "variant.hpp"
 #include "mapreplace.hpp"
+#include "filein.hpp"
+#include "readbuffer.hpp"
 #include <sys/utsname.h>
 #include <cstring>
 #include <cstdint>
@@ -18,6 +20,71 @@
 #ifdef __unix__
 #include <unistd.h>
 #endif
+
+using namespace Maike;
+
+static void trim(std::string & str)
+	{
+	while (str.length () > 0 && (str.back()>='\0' && str.back()<=' '))
+		{str.erase (str.length ()-1, 1);}
+	}
+
+static void cpufeaturesLoad(std::map<Stringkey,Variant>& variables)
+	{
+	FileIn source("/proc/cpuinfo");
+	ReadBuffer rb(source);
+	enum class State:unsigned int{KEY,VALUE};
+	auto state=State::KEY;
+	std::string buffer;
+	while(!rb.eof())
+		{
+		char ch_in=rb.byteRead();
+		switch(state)
+			{
+			case State::KEY:
+				switch(ch_in)
+					{
+					case ':':
+						state=State::VALUE;
+						trim(buffer);
+						printf("[%s] ",buffer.c_str());
+						buffer.clear();
+						break;
+					case '\n':
+						return; //Stop after reading one node
+					default:
+						if(ch_in>='\0' && ch_in<=' ')
+							{
+							if(buffer.length()>0)
+								{buffer+=ch_in;}
+							}
+						else
+							{buffer+=ch_in;}
+					}
+				break;
+
+			case State::VALUE:
+				switch(ch_in)
+					{
+					case '\n':
+						trim(buffer);
+						printf("[%s]\n",buffer.c_str());
+						buffer.clear();
+						state=State::KEY;
+						break;
+					default:
+						if(ch_in>='\0' && ch_in<=' ')
+							{
+							if(buffer.length()>0)
+								{buffer+=ch_in;}
+							}
+						else
+							{buffer+=ch_in;}
+					}
+				break;
+			}
+		}
+	}
 
 void Maike::sysvarsLoad(std::map<Stringkey, Variant>& variables)
 	{
@@ -37,4 +104,6 @@ void Maike::sysvarsLoad(std::map<Stringkey, Variant>& variables)
 #endif
 
 	replace(variables,{Stringkey("nullfile"),"/dev/null"});
+
+	cpufeaturesLoad(variables);
 	}
