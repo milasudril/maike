f920f86a3a1769e982b17b5bc99ef05f508f5c91
Torbjörn Rathsman <milasudril@gmail.com>
2016-07-22 09:37:09 +0200

    Added module for pkgconfig processing

---
+++ targetcxxpkgconfig.hpp
@@ -0,0 +1,20 @@
+//@	{
+//@	"targets":[{"name":"targetcxxpkgconfig.hpp","type":"include"}]
+//@	,"dependencies_extra":[{"ref":"targetcxxpkgconfig.o","rel":"implementation"}]
+//@	}
+
+#ifndef MAIKE_TARGETCXXPKGCONFIG_HPP
+#define MAIKE_TARGETCXXPKGCONFIG_HPP
+
+#include "visibility.hpp"
+
+namespace Maike
+	{
+	class Command;
+	class Target;
+	class TargetCxxOptions;
+ 	PRIVATE void pkgconfigAsk(const Command& cmd,const char* libname
+		,Target& target,TargetCxxOptions& options);
+	}
+
+#endif
