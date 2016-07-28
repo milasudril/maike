41d77d63e06406bb0381f3224407057e2d99bf7d
Torbjörn Rathsman <milasudril@gmail.com>
2016-07-28 14:31:24 +0200

    Added more alternatives for finding plugins

--- plugin-linuxgnu.cpp
+++ plugin-linuxgnu.cpp
@@ -17,16 +17,20 @@
 #include "exceptionhandler.hpp"
 #include "strerror.hpp"
 #include "pathutils.hpp"
+#include "filein.hpp"
+#include "readbuffer.hpp"
 
 #include <dlfcn.h>
 #include <limits.h>
 #include <unistd.h>
 #include <cstring>
+#include <vector>
 
 using namespace Maike;
 
 static std::string exename()
 	{
+//TODO: This function belongs to pathutils
 	ssize_t N=1;
 	ssize_t n=0;
 	std::string ret;
@@ -41,17 +45,101 @@ static std::string exename()
 				,{static_cast<const char*>(Maike::strerror(errno))}));
 			}
 		}
+	ret.resize(n);
 	return std::move(ret);
+	}
+
+static std::string getcwd()
+	{
+//TODO: This function belongs to pathutils
+	ssize_t N=1;
+	std::string ret;
+	char* result=NULL;
+	while(result==NULL)
+		{
+		ret.resize(N);
+		result=getcwd(&ret[0],N+1);
+		N<<=1;
+		}
+	ret.resize(strlen(ret.c_str()));
+	return std::move(ret);
+	}
+
+static std::string homedir()
+	{
+//TODO: This function belongs to pathutils
+	FileIn src("/etc/passwd");
+	ReadBuffer rb(src);
+	std::string field;
+	auto uid=getuid();
+	decltype(uid) uid_from_file;
+	std::string ret;
+	size_t field_count=0;
+	while(!rb.eof())
+		{
+		auto ch_in=rb.byteRead();
+		switch(ch_in)
+			{
+			case ':':
+				switch(field_count)
+					{
+					case 2:
+						uid_from_file=atoi(field.c_str());
+						break;
+					case 5:
+						ret=field;
+						break;
+					default:
+						break;
+					}
+				++field_count;
+				field.clear();
+				break;
+			case '\n':
+				if(uid_from_file==uid)
+					{return std::move(ret);}
+				field_count=0;
+				break;
+			default:
+				field+=ch_in;
+			}
+		}
+	return std::string("");
+	}
+
+static void* open(const std::vector<std::string>& names_to_try,std::string& name)
+	{
+	auto ptr=names_to_try.data();
+	auto ptr_end=ptr + names_to_try.size();
+	void* ret=NULL;
+	while(ptr!=ptr_end)
+		{
+		ret=dlopen(ptr->c_str(),RTLD_NOW|RTLD_DEEPBIND);
+		if(ret!=NULL)
+			{
+			name=*ptr;
+			return ret;
+			}
+		++ptr;
+		}
+	return ret;
 	}
 
 Plugin::Plugin(const char* name):m_name(name)
 	{
+	std::vector<std::string> names_to_try;
 	if(strchr(name,'/')==nullptr)
-		{m_name_full=dircat(dirname(exename()),name);}
+		{
+		names_to_try.push_back(dircat(dirname(exename()),name)+".so");
+		names_to_try.push_back(dircat(std::string("/usr/local/lib/maike"),name)+".so");
+		names_to_try.push_back(dircat(std::string("/usr/lib/maike"),name)+".so");
+		names_to_try.push_back(dircat(dircat(homedir(),"lib/maike"),name)+".so");
+		names_to_try.push_back(dircat(getcwd(),name)+".so");
+		}
 	else
-		{m_name_full=name;}
-	m_name_full+=".so";
-	m_handle=dlopen(m_name_full.c_str(),RTLD_NOW|RTLD_DEEPBIND);
+		{names_to_try.push_back(m_name+".so");}
+
+	m_handle=open(names_to_try,m_name_full);
 	if(m_handle==NULL)
 		{
 		exceptionRaise(ErrorMessage("It was not possible to load #0;. #1;"
