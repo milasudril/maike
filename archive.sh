58c115c89f25e65cce30ad154e8826a1c26eb760
Torbjörn Rathsman <milasudril@gmail.com>
2017-09-03 16:59:03 +0200

    Fixed #61

--- archive.sh
+++ archive.sh
@@ -13,8 +13,10 @@ dir_target="$1"
 
 cp "$dir_target"/externals.json externals-in.json
 
-find . | grep -v '^.$' | grep -v '__.*' | grep -v 'gh-pages' | grep -v 'debian' \
-	| grep -v '/\..*' | grep -v '.*\.blend1' \
+find . -mindepth 1 '(' -false -or -name '.*'\
+	 -or -name 'debian' \
+	 -or -name 'gh-pages' \
+	 -or -name '__*'  ')' -prune -or -print \
 	| tar '--transform=s,^\.,maike-src,' -czf "$dir_target"/maike-src.tar.gz -T -
 
 trap : 0
