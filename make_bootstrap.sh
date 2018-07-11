ef7ce6c180e99294319e6912fccad4e70dfabf2a
Torbjörn Rathsman <milasudril@gmail.com>
2018-07-11 19:48:19 +0200

    Trying to make g++ alias work

--- make_bootstrap.sh
+++ make_bootstrap.sh
@@ -4,13 +4,15 @@ cat << 'EOF' > bootstrap
 #!/bin/bash
 set -eo pipefail
 
-CXX_VERSION=$(g++ -dM -E -x c++ /dev/null | grep __cplusplus | awk '{print $3}' | sed 's/L//g')
-if [ "$CXX_VERSION" -le "201103" ]; then
-	echo "Trying to enable c++11"
+shopt -s expand_aliases
+
+CXX_VERSION=$('g++' -dM -E -x c++ /dev/null | grep __cplusplus | awk '{print $3}' | sed 's/L//g')
+if [ "$CXX_VERSION" -lt "201103" ]; then
+	echo "Current compiler defaults to $CXX_VERSION. Trying to enable c++11"
 	alias g++='g++ -std=c++11'
 fi
 EOF
 
-maike >> bootstrap
+maike | sed "s/^'g++'/g++/" >> bootstrap
 chmod u+x bootstrap
 cp bootstrap build.sh
