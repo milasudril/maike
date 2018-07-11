d02171533c85fc250d7167e8b21bebdc23714883
Torbjörn Rathsman <milasudril@gmail.com>
2018-07-11 21:14:39 +0200

    Fixed dynamic C++ upgrade in bootstrap

--- make_bootstrap.sh
+++ make_bootstrap.sh
@@ -8,7 +8,7 @@ shopt -s expand_aliases
 
 CXX_VERSION=$('g++' -dM -E -x c++ /dev/null | grep __cplusplus | awk '{print $3}' | sed 's/L//g')
 if [ "$CXX_VERSION" -lt "201103" ]; then
-	echo "Current compiler defaults to $CXX_VERSION. Trying to enable c++11"
+	echo "Current compiler defaults to $CXX_VERSION. Trying to enable C++11."
 	alias g++='g++ -std=c++11'
 fi
 EOF
