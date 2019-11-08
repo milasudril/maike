b5431a1194f6b02f4d4b3f70ff518fae7a731bbd
Torbjörn Rathsman <milasudril@gmail.com>
2019-11-08 21:52:19 +0100

    Register targettrivial by default

--- target_hook_registry.cpp
+++ target_hook_registry.cpp
@@ -181,6 +181,13 @@ Target_Hook_Registry& Target_Hook_Registry::configAppendDefault()
 		hook_info.filename_exts.insert(std::string(".cp"));
 		hook_info.hook->configAppendDefault();
 		}
+
+		{
+		auto& hook_info=hookCreate("targettrivial_default","targettrivial");
+		r_filenameext_hook[Stringkey(".*")]=hook_info.hook.get();
+		hook_info.filename_exts.insert(std::string(".*"));
+		hook_info.hook->configAppendDefault();
+		}
 	return *this;
 	}
 
