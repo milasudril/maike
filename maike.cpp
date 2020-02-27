5c69b931b1d51246a4d164c276df99dec1ece0cf
Torbjörn Rathsman <milasudril@gmail.com>
2020-02-27 21:48:50 +0100

    Add posix regex filter

--- maike.cpp
+++ maike.cpp
@@ -1,6 +1,8 @@
 //@	 {"targets":[{"name":"maike_next","type":"application"}]}
 
 #include "sourcefile.hpp"
+#include <set>
+#include <regex>
 
 template<class T>
 decltype(auto) pop(T& stack)
@@ -15,6 +17,9 @@ namespace fs = std::experimental::filesystem;
 int main()
 {
 	std::stack<Maike::Path> paths_to_visit;
+	std::vector<std::regex> ignore;
+	ignore.push_back(std::regex{"/\\..*", std::regex_constants::basic});
+	ignore.push_back(std::regex{"/__*.*", std::regex_constants::basic});
 	paths_to_visit.push(Maike::Path{"."});
 	while(!paths_to_visit.empty())
 	{
@@ -23,8 +28,13 @@ int main()
 		if(is_directory(test.name()))
 		{
 			auto i = fs::directory_iterator(test.name());
-			std::for_each(begin(i), end(i), [&paths_to_visit](auto const& item) {
-				paths_to_visit.push(item.path());
+			std::for_each(begin(i), end(i), [&paths_to_visit, &ignore](auto const& item) {
+				if(std::none_of(std::begin(ignore), std::end(ignore), [&item](auto const& regex){
+					return regex_search(item.path().c_str(), regex);
+				}))
+				{
+					paths_to_visit.push(item.path());
+				}
 			});
 		}
 	}
