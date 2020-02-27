246c222a4ffb1901959f8073484769cbda4cb3e0
Torbjörn Rathsman <milasudril@gmail.com>
2020-02-27 22:14:22 +0100

    Fiddeling with filename extensions

--- sourcefile.cpp
+++ sourcefile.cpp
@@ -2,8 +2,29 @@
 
 #include "./sourcefile.hpp"
 
+namespace
+{
+	std::string getExtension(Maike::Path const& path)
+	{
+		auto filename = path.filename().c_str();
+		while(true)
+		{
+			auto ch_in = *filename;
+			if(ch_in == 0)
+			{
+				return "";
+			}
+			if(ch_in == '.')
+			{
+				return filename;
+			}
+			++filename;
+		}
+	}
+}
+
 Maike::SourceFile::SourceFile(Path&& src):
 m_name{std::move(src)}
 {
-	printf("%s\n", m_name.c_str());
+	printf("%s  %s\n", m_name.c_str(), getExtension(m_name).c_str());
 }
