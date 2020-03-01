8359d47bbdb37f8f0b46301b0f3103b17cc07677
Torbjörn Rathsman <milasudril@gmail.com>
2020-03-01 15:02:39 +0100

    Activate memcheck

--- tester.sh
+++ tester.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+valgrind --leak-check=full -q --error-exitcode=255 --gen-suppressions=all --track-fds=yes "$1" && \
 valgrind --tool=helgrind -q --error-exitcode=255 --sim-hints=no-nptl-pthread-stackcache "$1"
 
-#valgrind --leak-check=full -q --error-exitcode=255 --gen-suppressions=all --track-fds=yes "$1" && \
