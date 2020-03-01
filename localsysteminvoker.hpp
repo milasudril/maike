08855cc87fb6109ffa034eedc46463c01fa0ff6b
Torbjörn Rathsman <milasudril@gmail.com>
2020-03-01 17:57:25 +0100

    Initial work on LocalSystemInvoker

---
+++ localsysteminvoker.hpp
@@ -0,0 +1,43 @@
+//@	{
+//@	 "targets":[{"name":"localsysteminvoker.hpp","type":"include"}]
+//@	 }
+
+#ifndef MAIKE_LOCALSYSTEMINVOKER_HPP
+#define MAIKE_LOCALSYSTEMINVOKER_HPP
+
+#include "./fs.hpp"
+#include "./command.hpp"
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+namespace Maike
+{
+	class LocalSystemInvoker
+	{
+	public:
+		void mkdir(fs::path const& dir) const
+		{
+			if(mkdir(dir.c_str(), S_IRUSR | S_IWUSR | S_IXUSR) == -1)
+			{ throw std::runtime_error{"Failed to create directory"}; }
+		}
+
+		int writeToFile(std::string const& str, fs::path const& file) const;
+
+		void copy(fs::path const& src, fs::path const& dest) const
+		{
+			std::error_code sc;
+			if(!copy_file(src, dest, ec)) { throw std::runtime_error{"Failed to copy file"}; }
+		}
+
+		template<class IoRedirector>
+		decltype(auto) execp(Command const& cmd, IoRedirector&& redir) const
+		{
+			return cmd.execp(redir);
+		}
+
+	private:
+	};
+}
+
+#endif
