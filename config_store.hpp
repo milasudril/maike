878e5e7e1b2a29046d0bc67c17a405cbee456ea6
Torbjörn Rathsman <milasudril@gmail.com>
2020-03-05 21:31:52 +0100

    Add methods to ConfigStore

--- config_store.hpp
+++ config_store.hpp
@@ -10,6 +10,16 @@ namespace Maike
 	class ConfigStore
 	{
 	public:
+		ConfigStore(ConfigStore&&);
+		ConfigStore& operator=(ConfigStore&&);
+		~ConfigStore();
+
+		template<class T>
+		T const& get(char const* key);
+
+		template<class T>
+		ConfigStore& insert(char const* key, T&& value);
+
 	private:
 	};
 }
