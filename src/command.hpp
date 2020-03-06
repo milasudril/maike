975895a0c9380728b701266fb0648ef1d2160120
Torbjörn Rathsman <mialsudril@gmail.com>
2020-03-06 19:48:49 +0100

    Verify that VcsInvoker config is git by default

--- src/command.hpp
+++ src/command.hpp
@@ -14,25 +14,35 @@ namespace Maike
 	{
 	public:
 		explicit Command(fs::path&& exe, std::vector<std::string>&& args):
-			m_executable{std::move(exe)}, m_args{std::move(args)}
-			{}
-		
+		   m_executable{std::move(exe)},
+		   m_args{std::move(args)}
+		{
+		}
+
 		fs::path const& executable() const
-		{ return m_executable; }
-		
+		{
+			return m_executable;
+		}
+
 		std::vector<std::string> const& args() const
-		{ return m_args; }
+		{
+			return m_args;
+		}
 
 	private:
 		fs::path m_executable;
 		std::vector<std::string> m_args;
 	};
-	
+
 	bool operator==(Command const& a, Command const& b)
-	{ return a.executable() == b.executable() && a.args() == b.args(); }
-	
+	{
+		return a.executable() == b.executable() && a.args() == b.args();
+	}
+
 	bool operator!=(Command const& a, Command const& b)
-	{ return !(a == b); }
+	{
+		return !(a == b);
+	}
 }
 
 #endif
--- src/vcs_invoker/config.test.cpp
+++ src/vcs_invoker/config.test.cpp
@@ -1,12 +1,19 @@
 //@	{"targets":[{"name":"config.test","type":"application", "autorun":1}]}
 
+#undef NDEBUG
+
 #include "./config.hpp"
+#include <cassert>
 
 namespace Testcases
 {
 	void maikeVcsInvokerConfigDefaultGit()
 	{
 		Maike::VcsInvoker::Config cfg;
+
+		assert((cfg.getRevision() == Maike::Command{"git", {"rev-parse", "HEAD"}}));
+		assert((cfg.getVersionTag() == Maike::Command{"git", {"describe"}}));
+		assert((cfg.getBranch() == Maike::Command{"git", {"rev-parse", "--abbrev-ref", "HEAD"}}));
 	}
 }
 
