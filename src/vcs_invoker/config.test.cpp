a8696be759f387ef2232bd9cc3a5acfec2d0ed0c
Torbjörn Rathsman <mialsudril@gmail.com>
2020-03-06 20:03:00 +0100

    Testcases for VcsInvoker config

--- src/vcs_invoker/config.test.cpp
+++ src/vcs_invoker/config.test.cpp
@@ -15,10 +15,51 @@ namespace Testcases
 		assert((cfg.getVersionTag() == Maike::Command{"git", {"describe"}}));
 		assert((cfg.getBranch() == Maike::Command{"git", {"rev-parse", "--abbrev-ref", "HEAD"}}));
 	}
+	
+	void maikeVcsInvokerSetGetRevision()
+	{
+		Maike::VcsInvoker::Config cfg;
+		auto get_version_tag = cfg.getVersionTag();
+		auto get_branch = cfg.getBranch();
+		
+		cfg.getRevision(Maike::Command{"foo", {"bar", "kaka"}});
+		
+		assert((cfg.getRevision() == Maike::Command{"foo", {"bar", "kaka"}}));
+		assert(cfg.getVersionTag() == get_version_tag);
+		assert(cfg.getBranch() == get_branch);
+	}
+	
+	void maikeVcsInvokerSetGetVersionTag()
+	{
+		Maike::VcsInvoker::Config cfg;
+		auto get_revision = cfg.getRevision();
+		auto get_branch = cfg.getBranch();
+		cfg.getVersionTag(Maike::Command{"foo", {"bar", "kaka"}});
+
+		assert((cfg.getVersionTag() == Maike::Command{"foo", {"bar", "kaka"}}));
+		assert(cfg.getRevision() == get_revision);
+		assert(cfg.getBranch() == get_branch);
+	}
+	
+	
+	void maikeVcsInvokerSetGetBranch()
+	{
+		Maike::VcsInvoker::Config cfg;
+		auto get_revision = cfg.getRevision();
+		auto get_version_tag = cfg.getVersionTag();
+		
+		cfg.getBranch(Maike::Command{"foo", {"bar", "kaka"}});
+		assert((cfg.getBranch() == Maike::Command{"foo", {"bar", "kaka"}}));
+		assert(cfg.getRevision() == get_revision);
+		assert(cfg.getVersionTag() == get_version_tag);
+	}
 }
 
 int main()
 {
 	Testcases::maikeVcsInvokerConfigDefaultGit();
+	Testcases::maikeVcsInvokerSetGetRevision();
+	Testcases::maikeVcsInvokerSetGetVersionTag();
+	Testcases::maikeVcsInvokerSetGetBranch();
 	return 0;
 }
