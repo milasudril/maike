f27343ee5454b5782c6a2f303a83a2c5119e5698
Torbjörn Rathsman <milasudril@gmail.com>
2020-03-28 09:09:19 +0100

    Add Writer interface

---
+++ src/writer.hpp
@@ -0,0 +1,52 @@
+//@	{
+//@	 "targets":[{"name":"writer.hpp","type":"include"}]
+//@	}
+
+#ifndef MAIKE_WRITER_HPP
+#define MAIKE_WRITER_HPP
+
+#include <cstddef>
+#include <type_traits>
+
+namespace Maike
+{
+	namespace writer_impl
+	{
+		template<class Source>
+		void do_write(Source& src, std::byte const* buffer, size_t n)
+		{
+			write(src, buffer, n);
+		}
+	}
+
+	class Writer
+	{
+	public:
+		template<class Source>
+		explicit Writer(Source&& src):
+		   r_source{&src},
+		   r_callback{[](void* src, std::byte const* buffer, size_t n) {
+			   using Src = std::decay_t<Source>;
+			   auto& self = *reinterpret_cast<Src*>(src);
+			   writer_impl::do_write(self, buffer, n);
+		   }}
+		{
+		}
+
+		void write(std::byte const* buffer, size_t n)
+		{
+			r_callback(r_source, buffer, n);
+		}
+
+	private:
+		void* r_source;
+		void (*r_callback)(void* src, std::byte const* buffer, size_t n);
+	};
+
+	void write(Writer writer, std::byte const* buffer, size_t n)
+	{
+		writer.write(buffer, n);
+	}
+}
+
+#endif
---
+++ src/writer.test.cpp
@@ -0,0 +1,44 @@
+//@	{
+//@	 "targets":[{"name":"writer.test","type":"application", "autorun":1}]
+//@	}
+
+#undef NDEBUG
+
+#include "./writer.hpp"
+
+#include <cassert>
+#include <string_view>
+#include <array>
+#include <cstring>
+
+namespace
+{
+	struct Sink
+	{
+		std::string buffer;
+	};
+
+	void write(Sink& dest, std::byte const* buff, size_t n)
+	{
+		dest.buffer.append(reinterpret_cast<char const*>(buff), n);
+	}
+}
+
+namespace Testcases
+{
+	void maikeWriterReadStuff()
+	{
+		Sink dest;
+		Maike::Writer writer{dest};
+		write(dest, reinterpret_cast<std::byte const*>("Hello"), 5);
+		assert(dest.buffer == "Hello");
+		write(dest, reinterpret_cast<std::byte const*>(", World"), 7);
+		assert(dest.buffer == "Hello, World");
+	}
+}
+
+int main()
+{
+	Testcases::maikeWriterReadStuff();
+	return 0;
+}
