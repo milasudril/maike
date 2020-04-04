96ddb1b3ba030b9361977415941214d3e244aa63
Torbjörn Rathsman <milasudril@gmail.com>
2020-04-04 21:20:43 +0200

    Output buffer

---
+++ src/output_buffer.hpp
@@ -0,0 +1,56 @@
+//@	{
+//@	 "targets":[{"name":"output_buffer.hpp","type":"include"}]
+//@	}
+
+#ifndef MAIKE_OUTPUTBUFFER_HPP
+#define MAIKE_OUTPUTBUFFER_HPP
+
+#include <cstddef>
+#include <array>
+
+namespace Maike
+{
+	template<class Sink, size_t Capacity = 4096>
+	class OutputBuffer
+	{
+		public:
+			OutputBuffer(OutputBuffer&&) = delete;
+			OutputBuffer& operator=(OutputBuffer&&) = delete;
+
+			explicit OutputBuffer(Sink sink):r_write_ptr{m_buffer.data()}, m_sink{std::move(sink)}
+			{}
+
+			OutputBuffer& putchar(std::byte byte)
+			{
+				if(r_write_ptr == m_buffer.end())
+				{
+					flush();
+				}
+				*r_write_ptr = byte;
+				++r_write_ptr;
+				return *this;
+			}
+
+			void flush();
+
+			~OutputBuffer()
+			{flush();}
+
+			Sink const& sink() const
+			{return m_sink;}
+
+		private:
+			std::byte* r_write_ptr;
+			Sink m_sink;
+			std::array<std::byte, Capacity> m_buffer;
+	};
+
+	template<class Sink, size_t Capacity>
+	void OutputBuffer<Sink, Capacity>::flush()
+	{
+		(void)write(m_sink, m_buffer.data(), m_buffer.size());
+		r_write_ptr = m_buffer.data();
+	}
+}
+
+#endif
---
+++ src/output_buffer.test.cpp
@@ -0,0 +1,47 @@
+//@	{
+//@	 "targets":[{"name":"output_buffer.test","type":"application", "autorun":1}]
+//@	}
+
+#undef NDEBUG
+
+#include "output_buffer.hpp"
+
+#include <cassert>
+#include <vector>
+#include <functional>
+#include <cstring>
+
+namespace
+{
+	struct Sink
+	{
+		std::vector<std::byte> data;
+	};
+
+	void write(Sink& sink, std::byte const* buffer, size_t n)
+	{
+		sink.data.insert(std::end(sink.data), buffer, buffer + n);
+	}
+}
+
+namespace Testcases
+{
+	void maikeOutputBufferWrite()
+	{
+		Sink test;
+		Maike::OutputBuffer<Sink, 4> buffer{std::ref(test)};
+		char const* input = "Hello, World";
+		while(*input != 0)
+		{
+			buffer.putchar(static_cast<std::byte>(*input));
+			++input;
+		}
+		test.data.push_back(static_cast<std::byte>(0));
+		assert(strcmp("Hello, World", reinterpret_cast<char const*>(test.data.data())));
+	}
+}
+
+int main()
+{
+	Testcases::maikeOutputBufferWrite();
+}
