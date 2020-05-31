533a466bdea20e8a8aecb910482aad57316c2c89
Torbjörn Rathsman <milasudril@gmail.com>
2020-05-31 12:25:07 +0200

    Add UnresolvedDep

---
+++ src/db/unresolved_dependency.hpp
@@ -0,0 +1,55 @@
+//@	{
+//@	 "targets":[{"name":"unresolved_dependency.hpp","type":"include"}]
+//@	 }
+
+#ifndef MAIKE_DB_UNRESOLVEDDEPENDENCY_HPP
+#define MAIKE_DB_UNRESOLVEDDEPENDENCY_HPP
+
+#include "src/fs.hpp"
+
+namespace Maike::Db
+{
+	class UnresolvedDependency
+	{
+	public:
+		enum class Resolver : int
+		{
+			InternalLookup,
+			None
+		};
+
+		explicit UnresolvedDependency(fs::path const& path, Resolver res_method):
+		   m_name{path},
+		   r_srcfile{nullptr},
+		   m_res_method{res_method}
+
+		{
+		}
+
+		fs::path const& name() const
+		{
+			return m_name;
+		}
+
+		Resolver resolver() const
+		{
+			return m_res_method;
+		}
+
+	private:
+		fs::path m_name;
+		Resolver m_res_method;
+	};
+
+	inline bool operator==(UnresolvedDependency const& a, UnresolvedDependency const& b)
+	{
+		return a.name() == b.name() && a.resolver() == b.resolver();
+	}
+
+	inline bool operator!=(UnresolvedDependency const& a, UnresolvedDependency const& b)
+	{
+		return !(a == b);
+	}
+}
+
+#endif
