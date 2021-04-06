f337e971d83cc8dc05ea02288757a8560d446657
Torbjörn Rathsman <milasudril@gmail.com>
2021-04-06 09:38:42 +0200

    Existing directories are always up-to-date

--- src/db/target_info.cpp
+++ src/db/target_info.cpp
@@ -10,6 +10,9 @@ bool Maike::Db::isUpToDate(TargetInfo const& target,
                            fs::path const& src_file,
                            std::vector<Dependency> const& deps)
 {
+	std::error_code ec{};
+	if(is_directory(target.name(), ec)) { return true; }
+
 	return detail::call_is_older(src_file, target.name())
 	       && std::all_of(
 	          std::begin(deps), std::end(deps), [&target_name = target.name()](auto const& item) {
