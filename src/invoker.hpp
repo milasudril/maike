e8f51506bd1cbc26b0953d79d6b0481e756afbb2
Torbjörn Rathsman <milasudril@gmail.com>
2021-04-07 18:50:46 +0200

    More work on Invoker

--- src/invoker.hpp
+++ src/invoker.hpp
@@ -5,36 +5,24 @@
 #ifndef MAIKE_INVOKER_HPP
 #define MAIKE_INVOKER_HPP
 
-#include "./compilation_log.hpp"
-#include "src/key_value_store/compound.hpp"
 #include "utils/fs.hpp"
+#include "utils/varstring.hpp"
 
 #include <vector>
 #include <memory>
-#include <variant>
 
 namespace Maike
 {
-	class ExitSignal
-	{
-	public:
-		constexpr explicit ExitSignal(int value): m_value{value}
-		{
-		}
-
-		constexpr int value() const
-		{
-			return m_value;
-		}
-
-	private:
-		int m_value;
-	};
-
 	class ExitStatus
 	{
 	public:
-		constexpr explicit ExitSignal(int value): m_value{value}
+		enum class Type
+		{
+			ReturnValue,
+			Signal
+		};
+
+		constexpr explicit ExitSignal(Type type, int value): m_type{type}, m_value{value}
 		{
 		}
 
@@ -43,7 +31,13 @@ namespace Maike
 			return m_value;
 		}
 
+		constexpr Type type() const
+		{
+			return m_type;
+		}
+
 	private:
+		Type m_type;
 		int m_value;
 	};
 
@@ -51,7 +45,7 @@ namespace Maike
 	{
 		std::vector<std::byte> sysout;
 		std::vector<std::byte> syserr;
-		std::variant<ExitStatus, ExitSignal> exit_result;
+		ExitStatus exit_status;
 	};
 
 	class Invoker
@@ -59,14 +53,19 @@ namespace Maike
 	public:
 		void mkdir(fs::path const& dir) const
 		{
-			return m_handle->mkdir(fs::path const& dir);
+			return m_handle->mkdir(dir);
 		}
 
 		ExecResult execp(fs::path const& executable,
-		                 std::vector<std::string> const& args,
+		                 std::vector<Varstring> const& args,
 		                 std::vector<std::byte> const& sysin) const
 		{
 			return m_handle->execp(executable, args, sysin);
+		}
+
+		void setvar(std::string&& name, std::string&& value)
+		{
+			m_handle->setvar(std::move(name), std::move(value));
 		}
 
 	private:
@@ -76,8 +75,10 @@ namespace Maike
 			virtual void mkdir(fs::path const& dir) const = 0;
 
 			virtual ExecResult execp(fs::path const& executable,
-			                         std::vector<std::string> const& args,
+			                         std::vector<Varstring> const& args,
 			                         std::vector<std::byte> const& sysin) const = 0;
+
+			virtual void setvar(std::string&& name, std::string&& value) = 0;
 
 			virtual ~AbstractInvoker()
 			{
@@ -92,14 +93,19 @@ namespace Maike
 
 			void mkdir(fs::path const& dir) const override
 			{
-				return m_handle->mkdir(fs::path const& dir);
+				return m_obj.mkdir(dir);
 			}
 
 			ExecResult execp(fs::path const& executable,
-			                 std::vector<std::string> const& args,
+			                 std::vector<Varstring> const& args,
 			                 std::vector<std::byte> const& sysin) const override
 			{
-				return m_handle->execp(executable, args, sysin);
+				return m_obj.execp(executable, args, sysin);
+			}
+
+			void setvar(std::string&& name, std::string&& value) override
+			{
+				m_obj.setvar(name, value);
 			}
 
 			~InvokerImpl() override = default;
