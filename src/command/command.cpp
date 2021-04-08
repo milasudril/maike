5011ad6ece2a4c6fb16d2177e81bc48404409e02
Torbjörn Rathsman <milasudril@gmail.com>
2021-04-08 14:49:31 +0200

    Implement some evaluation functions

--- src/command/command.cpp
+++ src/command/command.cpp
@@ -4,7 +4,85 @@
 
 #include "./command.hpp"
 
-#include <string>
+#include <algorithm>
+
+Maike::CommandInterpreter::CommandOutput Maike::CommandInterpreter::expand(Literal const& obj)
+{
+	CommandOutput ret;
+	auto const& val = obj.value();
+	std::transform(std::begin(val), std::end(val), std::back_inserter(ret),
+					[](auto val){return static_cast<std::byte>(val);});
+	return ret;
+}
+
+Maike::CommandInterpreter::CommandOutput Maike::CommandInterpreter::expand(Command const& cmd)
+{
+	auto const& args = cmd.args();
+	std::vector<std::string> args_eval;
+	args_eval.reserve(std::size(args));
+	std::for_each(std::begin(args), std::end(args), [&args_eval](auto const& arg){
+		auto arg_eval = std::visit([](auto const& item){return makeEvaluatedArgument(item);}, arg);
+		std::copy(std::begin(arg_eval), std::end(arg_eval), std::back_inserter(arg_eval));
+	});
+	// TODO:
+	// return exep(cmd.executable(), args);
+	return CommandOutput{};
+}
+
+Maike::CommandInterpreter::EvaluatedArgument Maike::CommandInterpreter::makeEvaluatedArgument(CommandSplitOutput const& obj)
+{
+	auto const command_output = expand(obj.command());
+
+	EvaluatedArgument output_split;
+	std::string buffer;
+	auto ptr = std::begin(command_output);
+	auto const separator = obj.separator();
+	while(true)
+	{
+		auto ch_in = static_cast<char>(*ptr);
+		++ptr;
+		if(ch_in == separator)
+		{
+			output_split.push_back(std::move(buffer));
+		}
+		else
+		{
+			buffer += ch_in;
+		}
+	}
+	if(std::size(buffer) != 0)
+	{
+		output_split.push_back(std::move(buffer));
+	}
+
+	return output_split;
+}
+
+Maike::CommandInterpreter::EvaluatedArgument Maike::CommandInterpreter::makeEvaluatedArgument(ExpandString const& arg)
+{
+	auto const cmd_output = makeEvaluatedArgument(arg.command());
+
+	EvaluatedArgument ret;
+	std::transform(std::begin(cmd_output), std::end(cmd_output), std::back_inserter(ret), [&prefix = arg.prefix().value(),
+		&suffix = arg.suffix().value()
+	](auto const& item) {
+		return prefix + item + suffix;
+	});
+
+	return ret;
+}
+
+Maike::CommandInterpreter::CommandOutput Maike::CommandInterpreter::expand(Pipe const& pipe)
+{
+	auto const& commands = pipe.commands();
+	CommandOutput ret;
+	std::for_each(std::begin(commands), std::end(commands), [&ret](auto const& cmd) {
+		ret = std::visit([](auto const& item){return expand(item);}, cmd);
+	});
+
+	return ret;
+}
+
 
 #if 0
 Maike::CommandInterpreter::Command Maike::CommandInterpreter::makeCommand(char const* str)
--- src/command/command.hpp
+++ src/command/command.hpp
@@ -5,7 +5,6 @@
 
 #include "src/invoker.hpp"
 
-#include <memory>
 #include <string>
 #include <vector>
 #include <variant>
@@ -21,44 +20,82 @@ namespace Maike::CommandInterpreter
 	 * foo.o)
 	 */
 
+	using CommandOutput = std::vector<std::byte>;
+
+	using EvaluatedArgument = std::vector<std::string>;
+
 	class Literal
 	{
 	public:
+		explicit Literal(std::string&& val):m_value{std::move(val)}{}
+
+		std::string const& value() const { return m_value; }
+
 	private:
 		std::string m_value;
 	};
+
+	CommandOutput expand(Literal const& obj);
+
+	inline EvaluatedArgument makeEvaluatedArgument(Literal const& obj)
+	{return EvaluatedArgument{obj.value()};}
 
 	class ExpandString;
 
 	class Command
 	{
+	public:
+		std::string const& name() const { return m_name; }
+		std::vector<std::variant<Literal, ExpandString>> args() const { return m_args; }
+
 	private:
 		std::string m_name;
 		std::vector<std::variant<Literal, ExpandString>> m_args;
 	};
 
+	CommandOutput expand(Command const&);
+
 	class CommandSplitOutput
 	{
 	public:
+		Command const& command() const
+		{ return m_command; }
+
+		char separator() const
+		{ return m_separator; }
+
 	private:
 		Command m_command;
 		char m_separator;
 	};
 
+	EvaluatedArgument makeEvaluatedArgument(CommandSplitOutput const& obj);
+
 	class ExpandString
 	{
+	public:
+		Literal const& prefix() const { return m_prefix; }
+		CommandSplitOutput command() const { return m_command; }
+		Literal const& suffix() const { return m_suffix; }
+
 	private:
 		Literal m_prefix;
 		CommandSplitOutput m_command;
 		Literal m_suffix;
 	};
 
+	EvaluatedArgument makeEvaluatedArgument(ExpandString const& obj);
+
 	class Pipe
 	{
 	public:
+		auto const& commands() const { return m_commands; }
 	private:
 		std::vector<std::variant<Command, Literal>> m_commands;
 	};
+
+	CommandOutput expand(Pipe const& pipe);
+
 #if 0
 	struct SplitCommand
 	{
