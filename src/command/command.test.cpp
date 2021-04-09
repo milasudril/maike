a118f13cf34a94d11c260eeaa8eb3bf9b15b4a80
Torbjörn Rathsman <milasudril@gmail.com>
2021-04-09 14:36:01 +0200

    Handle more corner cases

--- src/command/command.test.cpp
+++ src/command/command.test.cpp
@@ -13,7 +13,7 @@ namespace Testcases
 		constexpr char const* test =
 		   "system.cat(foo.cpp)|system.g++(-, -x, c++, -std=c++17, foo{system.pkg-config(--libs, "
 		   "gtk+-3)/~ }"
-		   "bar, -o, foo.o, a{fo{o}(a, {test()}, b)}b, a{}b)";
+		   "bar, -o, foo.o, a{fo{o}(a, {test()/!}, {kaka()/%})/:}b, a{}b)";
 		auto pipe = Maike::CommandInterpreter::makePipe(test);
 
 		printf("%s\n", std::get_if<Maike::CommandInterpreter::Command>(&pipe.back())->name().c_str());
