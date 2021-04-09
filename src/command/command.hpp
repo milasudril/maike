8d5464d26a28763152fab5952434f4b186b8795b
Torbjörn Rathsman <milasudril@gmail.com>
2021-04-09 14:55:01 +0200

    Fix doc

--- src/command/command.hpp
+++ src/command/command.hpp
@@ -16,7 +16,7 @@ namespace Maike::CommandInterpreter
 	 *
 	 * Use ~ as escape symbol
 	 *
-	 * system.cat(foo.cpp)|system.g++(-, -x, c++, -std=c++17, system.pkg-config(--libs,gtk+-3)/~ , -o,
+	 * system.cat(foo.cpp)|system.g++(-, -x, c++, -std=c++17, {system.pkg-config(--libs,gtk+-3)/~ }, -o,
 	 * foo.o)
 	 */
 
--- src/command/syntax_draft.md
+++ src/command/syntax_draft.md
@@ -2,5 +2,5 @@ Basic syntax
 
 Use ~ as escape symbol
 
-system.cat(foo.cpp)|system.g++(-, -x, c++, -std=c++17, system.pkg-config(--libs, gtk+-3)/~ , -o, foo.o)
+    system.cat(foo.cpp)|system.g++(-, -x, c++, -std=c++17, {system.pkg-config(--libs, gtk+-3)/~ } , -o, foo.o)
 
