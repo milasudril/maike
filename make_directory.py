4cab3e0299f586a5e86ef9045f5a2109254fdf45
Torbjörn Rathsman <milasudril@gmail.com>
2021-04-25 08:51:21 +0200

    Basic implementation of make_directory

--- make_directory.py
+++ make_directory.py
@@ -2,6 +2,24 @@
 
 import sys
 import json
+import os
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+def mkdir(path):
+	try:
+		if path.endswith('/.'):
+			os.mkdir(path[0: -2])
+		else:
+			os.mkdir(path)
+	except FileExistsError:
+		pass
+
+def compile(dict):
+	for target in dict["targets"]:
+		mkdir(target)
 
 if __name__ == '__main__':
-	data = json.loads(sys.argv[2])
+	if sys.argv[1] == 'compile':
+		compile(json.loads(sys.argv[2]))
