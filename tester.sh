b04df261c775e66495480de8f67dd22905937a7d
Torbjörn Rathsman <milasudril@gmail.com>
2021-04-30 20:15:18 +0200

    Turn off valgrind (broken in Travis)

--- tester.sh
+++ tester.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-export test_exe="$1"
-valgrind --leak-check=full -q --error-exitcode=255 --gen-suppressions=all --track-fds=yes "$1" && \
-valgrind --tool=helgrind -q --error-exitcode=255 --gen-suppressions=all --suppressions=./helgrind.sup --sim-hints=no-nptl-pthread-stackcache "$1"
+"$1"
 
