9550c0c089616a3a377ff97c6e3996fb707eaa53
Torbjörn Rathsman <milasudril@gmail.com>
2021-05-02 11:10:20 +0200

    Implement complete support for C++ revision selection

--- recipes/cxx_compiler.py
+++ recipes/cxx_compiler.py
@@ -37,7 +37,8 @@ def compile(build_args):
 	args = []
 	args.append('g++')
 	args.extend(collect_cflags(build_args['compiler_cfg'], build_args['dependencies']))
-	args.append('-std=%s'%build_args['compiler_cfg']['std_revision']['selected'])
+	if 'selected' in build_args['compiler_cfg']['std_revision']:
+		args.append('-std=%s'%build_args['compiler_cfg']['std_revision']['selected'])
 	args.append('-fdiagnostics-color=%s'%('always' if build_args['log_format']=='ansi_term' else 'never'))
 	args.append('-c')
 	args.append(build_args['source_file'])
@@ -50,14 +51,61 @@ def compile(build_args):
 
 	return result.returncode
 
-rev_constants = {'c++98': 199711, 'c++11': 201103, 'c++14': 201402, 'c++17': 201703, 'c++17': 201702, 'c++20': 202002}
+def get_numeric_rev(rev):
+	rev_constants = {'c++98': 199711, 'c++11': 201103, 'c++14': 201402, 'c++17': 201703, 'c++17': 201702, 'c++20': 202002}
+	return rev_constants[rev]
+
+def get_literal_rev(rev):
+	rev_constants = {199711:'c++98', 201103:'c++11', 201402:'c++14', 201703:'c++17', 202002:'c++20'}
+	return rev_constants[rev]
+
+
+def get_default_ref():
+	args = ['g++', '-x', 'c++', '-E', '-dM', '/dev/null']
+	result = subprocess.run(args, stdin = subprocess.DEVNULL, stdout=subprocess.PIPE, text=True)
+	for line in result.stdout.split('\n'):
+		fields=line.split()
+		if len(fields) >= 3:
+			if fields[1] == '__cplusplus':
+				return int(fields[2][0:-1])
+
+
+def select_cpp_rev(rev):
+	if 'min' in rev:
+		min_num = get_numeric_rev(rev['min'])
+		if 'max' in rev:
+			max_num = get_numeric_rev(rev['max'])
+			if min_num > max_num:
+				eprint('Impossible configuration (min %s is newer than max %s)'%(rev['min'], rev['max']))
+				return 1
+			if rev['min'] == rev['max']:
+				rev['selected'] = rev['min']
+				return 0
+			else:
+				default_rev = get_default_ref()
+				if default_rev < min_num:
+					rev['selected'] = rev['min']
+					return 0
+				if default_rev > max_num:
+					rev['selected'] = rev['max']
+					return 0
+				return 0
+		else:
+			default_rev = get_default_ref()
+			if default_rev < min_num:
+				rev['selected'] = rev['min']
+				return 0
+	else:
+		if 'max' in rev:
+			default_rev = get_default_ref()
+			if default_rev > max_num:
+				rev['selected'] = rev['max']
+				return 0
 
 def configure(cfg):
 	if 'std_revision' in cfg:
-		rev = cfg['std_revision']
-		if rev['min'] == rev['max']:
-			rev['selected'] = rev['min']
-			print(json.dumps(cfg))
+		ret = select_cpp_rev(cfg['std_revision'])
+		print(json.dumps(cfg))
 		return 0
 	else:
 		return 0
