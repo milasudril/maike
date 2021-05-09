f8e0f1fd913275f053d7dc4d05c01637726c5e38
Torbjörn Rathsman <milasudril@gmail.com>
2021-05-09 12:16:17 +0200

    Reorder build deps

--- src/bin/about.cpp
+++ src/bin/about.cpp
@@ -2,7 +2,7 @@
 //@		"target":{"name":"about.o"},
 //@		"dependencies":
 //@		[
-//@			{"ref":"./about.hpp", "rel": "include", "origin":"project"},
-//@			{"ref":"./about_impl.hpp","rel":"include"}
+//@			{"ref":"./about_impl.hpp","rel":"include"},
+//@			{"ref":"./about.hpp", "rel": "include", "origin":"project"}
 //@		]
 //@	}
