3f0445f5e8bb886b9bcf19345c82d60f904d5667
Torbjörn Rathsman <milasudril@gmail.com>
2021-05-14 09:28:08 +0200

    Bootstrap from release script

--- integration_test/bootstrap_from_release.sh
+++ integration_test/bootstrap_from_release.sh
@@ -4,11 +4,10 @@ echo "Bootstraping from $bootsrap_from_version"
 
 tmp_dir=$(mktemp -d)
 trap "rm -rf $tmp_dir" EXIT
-echo $tmp_dir
 cd $tmp_dir
 wget "https://github.com/milasudril/maike/releases/download/$bootsrap_from_version/maike-src.tar.gz"
 tar -xf maike-src.tar.gz
 cd maike-src
-pwd
-make
+sh -x __buildcache/bootstrap.sh
+__targets_bootstrap/src/bin/maike2 --target-dir=__targets
 make PREFIX="$install_dir" install
