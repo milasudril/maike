7584f0b0dd085bff0e89e84c6a8a554c33182c35
Torbjörn Rathsman <milasudril@gmail.com>
2021-05-14 11:37:27 +0200

    Add new tc install_and_rebuild_out_of_tree (currently broken)

---
+++ integration_test/install_and_rebuild_out_of_tree.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Install and rebuild out of tree"
+
+install_dir_new=$(mktemp -d)
+build_dir=$(mktemp -d)
+trap "rm -rf $install_dir_new $build_dir" EXIT
+
+make PREFIX="$install_dir_new" install
+source_dir=$(pwd)
+cd $build_dir
+$install_dir_new/bin/maike2 --source-dir=$source_dir
--- integration_test/main.sh
+++ integration_test/main.sh
@@ -7,3 +7,4 @@ bootsrap_from_version="2.0.0-beta-5" integration_test/bootstrap_from_release.sh
 target_dir=__targets integration_test/build_new_version.sh
 integration_test/can_generate_working_bootstrap_script.sh __targets/src/bin/maike2
 integration_test/install_and_rebuild.sh
+#BROKEN: integration_test/install_and_rebuild_out_of_tree.sh
