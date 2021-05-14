90359c769feebd40b4518aaa8972387e0ef57b21
Torbjörn Rathsman <milasudril@gmail.com>
2021-05-14 09:53:34 +0200

    Validate build id

--- integration_test/bootstrap_from_release.sh
+++ integration_test/bootstrap_from_release.sh
@@ -9,5 +9,22 @@ wget "https://github.com/milasudril/maike/releases/download/$bootsrap_from_versi
 tar -xf maike-src.tar.gz
 cd maike-src
 sh -x __buildcache/bootstrap.sh
-__targets_bootstrap/src/bin/maike2 --target-dir=__targets
+build_id_from_bootstrap=$(__targets_bootstrap/src/bin/maike2 --target-dir=__targets \
+	| grep '#[[:space:]].*Id:' \
+	| awk 'BEGIN { FS = ":" } ; { print $2 }' \
+	| sed 's|[[:space:]]*||')
+
 make PREFIX="$install_dir" install
+build_id=$("$install_dir"/bin/maike2 --version \
+	| grep 'Build id' \
+	| awk 'BEGIN { FS = ":" } ; { print $2 }' \
+	| sed 's|[[:space:]]*||')
+
+if [[ "$build_id" == "$build_id_from_bootstrap" ]]; then
+	exit 0
+else
+	echo "BuildId mismatch:"
+	echo $build_id
+	echo $build_id_from_bootstrap
+	exit 1
+fi
--- integration_test/main.sh
+++ integration_test/main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export orig_version="2.0.0-beta-5"
 export install_dir=$(mktemp -d)
 trap "rm -rf $install_dir" EXIT
+
 bootsrap_from_version="2.0.0-beta-5" integration_test/bootstrap_from_release.sh
 
