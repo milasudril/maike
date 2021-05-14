85938abdaa58ff3dbfd968c81eb1e3100f3bf1e6
Torbjörn Rathsman <milasudril@gmail.com>
2021-05-14 18:15:53 +0200

    Add tc for #115

---
+++ integration_test/install_and_rebuild_out_of_tree_rel_path.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Install and rebuild out of tree (relative path)"
+
+install_dir_new=$(mktemp -d)
+build_dir=..
+trap "rm -rf $install_dir_new" EXIT
+
+make PREFIX="$install_dir_new" install
+source_dir=$(basename $(pwd))
+cd $build_dir
+$install_dir_new/bin/maike2 --source-dir=$source_dir
