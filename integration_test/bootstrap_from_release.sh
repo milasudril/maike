b5a17c3f347e122df6e62cfd05dd000af9fa02a3
Torbjörn Rathsman <milasudril@gmail.com>
2021-05-22 10:02:24 +0200

    Main executable was moved

--- integration_test/bootstrap_from_release.sh
+++ integration_test/bootstrap_from_release.sh
@@ -11,7 +11,7 @@ wget "https://github.com/milasudril/maike/releases/download/$bootsrap_from_versi
 tar -xf maike-src.tar.gz
 cd maike-src
 sh -x __buildcache/bootstrap.sh
-build_id_from_bootstrap=$(__targets_bootstrap/src/bin/maike2 --target-dir=__targets \
+build_id_from_bootstrap=$(__targets_bootstrap/core/bin/maike2 --target-dir=__targets \
 	| grep '#[[:space:]].*Id:' \
 	| awk 'BEGIN { FS = ":" } ; { print $2 }' \
 	| sed 's|[[:space:]]*||')
