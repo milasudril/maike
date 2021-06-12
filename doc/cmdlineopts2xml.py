36b405e7ef56e8cc53c48705d51c70b1addf9fa3
Torbjörn Rathsman <milasudril@gmail.com>
2021-06-12 10:17:07 +0200

    Remove extra stuff from manpage

---
+++ doc/cmdlineopts2xml.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+#	pandoc_res = subprocess.run(['pandoc', '-f', 'markdown', '-t', 'html', '--section-divs', '-o', targets[2]], input=content['opts'], text=True)
--- doc/manpage.py
+++ doc/manpage.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 
 #@	{
-#@	 "targets":
-#@		[
-#@			{"name":"maike2.1.md"},
-#@			{"name":"maike2.1.man"},
-#@			{"name":"cmdline.xml"}
-#@		]
+#@	 "target":
+#@			{"name":"maike2.1.man"}
 #@	,"dependencies":
 #@		[
 #@		{"ref":"./cmdlineopts.md"},
@@ -76,24 +72,22 @@ def get_copyright(projinfo_copyright):
 
 def compile(args):
 	target_dir = args['build_info']['target_dir']
+	src_dir = args['build_info']['source_dir']
 	targets = args['targets']
-	projinfo = load_projinfo(args['build_info']['source_dir'])
+	projinfo = load_projinfo(src_dir)
 	content = dict()
 	content['description'] = '\n\n'.join(projinfo['description'])
 	content['author'] = ', '.join(get_authors(projinfo['copyright']))
 	content['copyright'] = '\n\n'.join(get_copyright(projinfo['copyright']))
 	content['license_short'] = projinfo['legal_info']['license_short']
 
-	with open(target_dir + '/' + os.path.dirname(args['source_file']) +'/cmdlineopts.md') as f:
+	with open(target_dir + '/' + os.path.relpath(os.path.dirname(args['source_file']), src_dir) +'/cmdlineopts.md') as f:
 		content['opts'] = f.read()
 
 	timestamp = args['build_info']['start_time']
 	content['date'] = datetime.datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S UTC')
 	md_output = document.substitute(content)
-	with open(targets[0], 'w') as f:
-		f.write(md_output)
-	pandoc_res = subprocess.run(['pandoc', '-f', 'markdown', '-s', '-t', 'man', '-o', targets[1]], input=md_output, text=True)
-	pandoc_res = subprocess.run(['pandoc', '-f', 'markdown', '-t', 'html', '--section-divs', '-o', targets[2]], input=content['opts'], text=True)
+	pandoc_res = subprocess.run(['pandoc', '-f', 'markdown', '-s', '-t', 'man', '-o', targets[0]], input=md_output, text=True)
 	return 0
 
 if __name__ == '__main__':
