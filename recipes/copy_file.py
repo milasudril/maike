89010162405301406e95e603b5a086c782e185d7
Torbjörn Rathsman <milasudril@gmail.com>
2021-06-13 08:55:37 +0200

    Identity implemented

--- recipes/copy_file.py
+++ recipes/copy_file.py
@@ -5,11 +5,10 @@ import json
 import shutil
 
 def compile(args):
-	target_dir = args['build_info']['target_dir']
 	targets = args['targets']
 	source_file = args['source_file']
 	for target in targets:
-		print('%s %s'%(source_file, target))
+		shutil.copyfile(source_file, target)
 	return 0
 
 if __name__ == '__main__':
