21ffb0102ff2cdbc982aee4ff6a508c2e152745a
Torbjörn Rathsman <milasudril@gmail.com>
2022-02-20 10:52:18 +0100

    Make sure scripts have x flag

--- install.sh
+++ install.sh
@@ -8,4 +8,6 @@ dir=$1
 mkdir -p "$dir"/bin
 mkdir -p "$dir"/lib/maike2/recipes
 cp __targets/core/bin/maike2 "$dir"/bin
+chmod o+rx "$dir"/bin/maike2
 cp -r recipes/* "$dir"/lib/maike2/recipes
+chmod o+rx "$dir"/lib/maike2/recipes/*
